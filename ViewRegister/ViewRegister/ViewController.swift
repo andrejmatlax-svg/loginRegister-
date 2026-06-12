aa8ceee65e07b1476d057331165d171dd587a44e
Andrey <Matlah>
2026-06-12 21:40:36 +0300

    TextFiled - (userName)

--- ViewRegister/ViewRegister/ViewController.swift
+++ ViewRegister/ViewRegister/ViewController.swift
@@ -14,6 +14,36 @@ class ViewController: UIViewController {
             label.translatesAutoresizingMaskIntoConstraints = false
             return label
         }()
+        //MARK: TextFiled с названием (Username), с деталими (person, checkmark.circle.fill)
+            
+            private let usernameTextField: UITextField = {
+                let TextField = UITextField()
+                TextField.placeholder = "Username"
+                TextField.backgroundColor = .white
+                TextField.layer.cornerRadius = 22
+                TextField.textColor = .black
+                
+                
+                let leftView = UIView(frame: CGRect(x: 0, y: 0, width: 45, height: 44))
+                let icon = UIImageView(image: UIImage(systemName: "person"))
+                icon.tintColor = .lightGray
+                icon.frame = CGRect(x: 16, y: 13, width: 18, height: 18)
+                leftView.addSubview(icon)
+                TextField.leftView = leftView
+                TextField.leftViewMode = .always
+                
+               
+                let rightView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 44))
+                let checkIcon = UIImageView(image: UIImage(systemName: "checkmark.circle.fill"))
+                checkIcon.tintColor = .systemGreen
+                checkIcon.frame = CGRect(x: 6, y: 13, width: 18, height: 18)
+                rightView.addSubview(checkIcon)
+                TextField.rightView = rightView
+                TextField.rightViewMode = .always
+                TextField.translatesAutoresizingMaskIntoConstraints = false
+                return TextField
+            }()
+
         
         
     }
